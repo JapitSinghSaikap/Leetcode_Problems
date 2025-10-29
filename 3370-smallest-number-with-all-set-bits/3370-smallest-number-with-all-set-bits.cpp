20201bb930489a2edf9be398287267c40de14936
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2025-10-29 12:21:23 +0530

    Time: 0 ms (100%), Space: 8.2 MB (60.8%) - LeetHub

--- 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
+++ 3370-smallest-number-with-all-set-bits/3370-smallest-number-with-all-set-bits.cpp
@@ -1,13 +1,38 @@
-class Solution {
-public:
+// class Solution {
+// public:
 //the set bit numbers are 
 //1 3 5 7 15 31  toh pattern hai k*2+1
-    int smallestNumber(int n) {
-        int k=1;
-        while(k<n){
-            k=2*k+1;
-        }
+//     int smallestNumber(int n) {
+//         int k=1;
+//         while(k<n){
+//             k=2*k+1;
+//         }
 
-        return k;
+//         return k;
+//     }
+// };
+
+//n & (n+1) == 0 to check if num is pow of 2 - 1 (saare set bits honge iske)
+
+// class Solution {
+// public:
+// for 9 
+//O(logn)
+//O(1)
+//     int smallestNumber(int n) {
+//         while((n&(n+1))){
+//           n = n | n+1;
+//         }
+
+//         return n;
+//     }
+// };
+
+class Solution {
+public:
+    int smallestNumber(int n) {
+       vector<int> arr = {1,3, 7, 15, 31, 63, 127, 255, 511, 1023};
+       for(int it : arr) if(it>=n) return it;
+       return -1;
     }
 };
