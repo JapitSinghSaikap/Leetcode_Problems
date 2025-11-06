afcc29b2736baf310d84c9d74e343a35d95941f4
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2025-11-06 16:49:58 +0530

    Time: 4 ms (24.81%), Space: 27.8 MB (34.73%) - LeetHub

---
+++ 2932-maximum-strong-pair-xor-i/2932-maximum-strong-pair-xor-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumStrongPairXor(vector<int>& nums) {
+        int n = nums.size();
+        int maxVal = 0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(abs(nums[i]-nums[j])<=min(nums[i],nums[j])){
+                    int val = nums[i]^nums[j];
+                    maxVal = max(val,maxVal);
+                }
+            }
+        }
+        return maxVal;
+    }
+};
