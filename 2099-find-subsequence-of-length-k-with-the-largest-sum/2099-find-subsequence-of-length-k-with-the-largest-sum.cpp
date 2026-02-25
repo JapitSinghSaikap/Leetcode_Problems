c63485b7985d03249d63f11bb0e3c98c451d2ea7
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-02-25 18:28:22 +0530

    Time: 0 ms (100%), Space: 13.8 MB (57.76%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,37 +1,59 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> maxSubsequence(vector<int>& nums, int k) {
+        
+         
+//         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        
+//         for(int i = 0; i < nums.size(); i++){
+//             pq.push({nums[i], i});
+            
+//             if(pq.size() > k)
+//                 pq.pop();   // remove smallest
+//         }
+        
+//         // Store selected elements
+//         vector<pair<int,int>> temp;
+        
+//         while(!pq.empty()){
+//             temp.push_back(pq.top());
+//             pq.pop();
+//         }
+        
+//         // Sort by original index to maintain order
+//         sort(temp.begin(), temp.end(), [](auto &a, auto &b){
+//             return a.second < b.second;
+//         });
+        
+//         vector<int> res;
+//         for(auto &p : temp)
+//             res.push_back(p.first);
+        
+//         return res;
+//     }
+// };
+
 
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        
-        
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        
-        for(int i = 0; i < nums.size(); i++){
-            pq.push({nums[i], i});
-            
-            if(pq.size() > k)
-                pq.pop();   // remove smallest
+        int n = nums.size();
+        vector<pair<int, int>> vals;  // auxiliary array
+        for (int i = 0; i < n; ++i) {
+            vals.emplace_back(i, nums[i]);
         }
-        
-        // Store selected elements
-        vector<pair<int,int>> temp;
-        
-        while(!pq.empty()){
-            temp.push_back(pq.top());
-            pq.pop();
+        // sort by numerical value in descending order
+        sort(vals.begin(), vals.end(),
+             [&](auto x1, auto x2) { return x1.second > x2.second; });
+        // select the first k elements and sort them in ascending order by index
+        sort(vals.begin(), vals.begin() + k);
+        vector<int> res;  // target subsequence
+        for (int i = 0; i < k; ++i) {
+            res.push_back(vals[i].second);
         }
-        
-        // Sort by original index to maintain order
-        sort(temp.begin(), temp.end(), [](auto &a, auto &b){
-            return a.second < b.second;
-        });
-        
-        vector<int> res;
-        for(auto &p : temp)
-            res.push_back(p.first);
-        
         return res;
     }
 };
