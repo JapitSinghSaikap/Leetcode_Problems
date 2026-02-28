433e11766a5167828a462e626ebdada0fb048d5f
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-02-28 14:34:55 +0530

    Time: 0 ms (100%), Space: 8.1 MB (12.46%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int last = -1, maxDist = 0, i = 0;
+        while (n > 0) {
+            if (n & 1) {
+                if (last != -1) {
+                    maxDist = max(maxDist, i - last);
+                }
+                last = i;
+            }
+            n >>= 1;
+            i++;
+        }
+        return maxDist;
+    }
+};
