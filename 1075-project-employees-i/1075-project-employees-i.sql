cfe39e387273dba5aea2df1949ed0d9e80e6a572
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-03-10 00:10:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,7 +1,13 @@
-# Write your MySQL query statement below
+-- # Write your MySQL query statement below
 
-SELECT p.project_id, ROUND(AVG(experience_years),2) as average_years
+-- SELECT p.project_id, ROUND(AVG(experience_years),2) as average_years
+-- FROM Project p
+-- INNER JOIN Employee e
+-- ON p.employee_id = e.employee_id
+-- GROUP BY p.project_id;
+
+SELECT p.project_id, ROUND(AVG(experience_years),2)
 FROM Project p
-INNER JOIN Employee e
+JOIN Employee e
 ON p.employee_id = e.employee_id
 GROUP BY p.project_id;
