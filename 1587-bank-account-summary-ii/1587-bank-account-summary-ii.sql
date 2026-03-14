b55376adfecd588b92bec553d992176cb38b89e6
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-03-14 15:33:24 +0530

    Time: 728 ms (85.93%), Space: 0B (100%) - LeetHub

---
+++ 1587-bank-account-summary-ii/1587-bank-account-summary-ii.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT u.name, SUM(t.amount) as balance FROM Users u LEFT JOIN Transactions t ON u.account = t.account GROUP BY u.account HAVING SUM(t.amount) > 10000;
