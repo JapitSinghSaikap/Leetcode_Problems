9033a80e6b56d193aabcae45077720fac7594edd
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-03-16 14:46:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     long long countCommas(long long n) {
-        if(n < 1000){
+        if(n<1000){
             return 0;
         }
-        long long nv = n;
-        long long count = 0;
-        long long start = 1000;
-        while(start <= nv){
-            count+=nv-start+1;//count the comma's at that level
-            start*=1000;//move to the next range
+        long long e=n;
+        long long k=0;
+        long long s=1000;
+        while(s<=e){
+            k+=e-s+1;//count the comma's at that level
+            s*=1000;//move to the next range
         }
 
         return count;
