1ae1cd446a2f448dd0f9331ceefa0db59ec48c2a
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-03-30 15:18:43 +0530

    Time: 2 ms (37.82%), Space: 10.1 MB (38.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>>ans;
-    void solve(int index , vector<int>&nums , vector<int>&temp){
-        if(index==nums.size()){
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[index]);
-        solve(index+1,nums,temp);
-        temp.pop_back();//backtrack
-        solve(index+1,nums,temp);
+    void subsetRecur(int i, vector<int>& arr, 
+       vector<vector<int>>& res, vector<int>& subset) {
+    if (i == arr.size()) {
+        res.push_back(subset);
+        return;
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int>temp;
-        solve(0,nums,temp);
-        return ans;
+    //inlcluding the current value
+    subset.push_back(arr[i]);
+    subsetRecur(i+1, arr, res, subset);
+    //excluding the current value and uske baad saari explor ki
+    subset.pop_back();
+    subsetRecur(i+1, arr, res, subset);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& arr) {
+        vector<int> subset;
+        vector<vector<int>> res;
+        subsetRecur(0, arr, res, subset);
+        return res;
     }
 };
+
