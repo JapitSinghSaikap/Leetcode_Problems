339870548991600f09434adecdf31c1b43c7c53e
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-04 23:55:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,61 +1,20 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-         if (nums.empty()) return 0;
-
-        sort(nums.begin(), nums.end()); 
-        int maxlen = 1, len = 1;
-
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1])
-             continue; 
-            if (nums[i] == nums[i - 1] + 1) {
-                len++;
-            } else {
-                maxlen = max(maxlen, len);
-                len = 1;
-            }
+        unordered_set<int>st(nums.begin(),nums.end());
+        int max_count = 0;
+        for(int it : st){
+           if(!st.count(it-1)){
+                int el = it;
+                int curr_count = 0;
+                while(st.count(el)){
+                    curr_count++;
+                    el++;
+                }
+                max_count = max(max_count,curr_count);
+           }
         }
-        return max(maxlen, len);
+        return max_count;
+        
     }
 };
-
-
-// class Solution {
-// public:
-//     int longestConsecutive(vector<int>& nums) {
-//         int n=nums.size();
-//         if(n==0){
-//             return 0;
-//         }
-
-//         unordered_map<int,bool>mpp;
-//         int len=1; //to check for the seq
-//         int maxlen=1;//to check for the max seq
-        
-//         for(auto x: nums){
-//             mpp[x]=false;
-//         }
-        
-//         for(int i=0;i<n;i++){
-//             //next number konsa hona chahiye seq mein
-//             int nxt=nums[i]+1;
-
-//             while(mpp.find(nxt)!=mpp.end() && mpp[nxt]==false){
-//                 //ki yeh milgya hai
-//                 mpp[nxt]=true;
-//                 len++;
-//                 nxt++;//ab agla check kteinge
-//         }
-//         int prev=nums[i]-1;
-//         while(mpp.find(prev)!=mpp.end() && mpp[prev]==false){
-//                 mpp[prev]=true;
-//                 len++;
-//                 prev--;
-//             }
-//             maxlen=max(maxlen,len);
-//             len=1;
-//         }
-//         return maxlen;
-//     }
-// };
