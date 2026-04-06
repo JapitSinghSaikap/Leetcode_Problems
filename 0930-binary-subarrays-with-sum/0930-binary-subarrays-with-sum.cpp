5ba86e6706c978227e888190c21b886e41bb1ebc
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-06 20:21:27 +0530

    Time: 21 ms (22.38%), Space: 38.8 MB (18.6%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    //prefix sum se kiya hai yeh
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         unordered_map<int,int> mpp;
         int sum=0;
         int finalSum=0;
         for(auto it:nums){
+            //yhan pe hum prefix sum hi bna rhe 
             sum+=it;
             if(sum==goal){
                 finalSum++;
             }
+            //and if part mil gya mpp mein 
             if(mpp.find(sum-goal)!=mpp.end()){
                 //yeh prefix sum wala part hai
                finalSum+=mpp[sum-goal];
