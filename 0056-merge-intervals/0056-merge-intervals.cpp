29c3ddbc3e1b1b6fa66ffe9020d4ad9686e869ca
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-11 14:59:47 +0530

    Time: 101 ms (5.16%), Space: 24.9 MB (8.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
+        for(auto it: intervals){
+            cout<<it[0]<<" "<<it[1]<<endl;
+        }
         vector<vector<int>> merged;
         vector<int> prev = intervals[0];
 
@@ -11,6 +14,10 @@ public:
                 prev[1] = max(prev[1], intervals[i][1]);
             } else {
                 merged.push_back(prev);
+                //agr overlap na ho then make the new prev for ex [1,6] [8,10]  
+                // 8<=6 Nhi
+                //push [1,6] to merged and new prev is 
+                //prev 
                 prev = intervals[i];
             }
         }
