5a9e0a19ce1d90bd4d5bfcb7db523b09e9dc848c
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-11 18:35:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,8 +1,44 @@
+// class Solution {
+// public:
+// //O(n) approach
+//     int search(vector<int>& nums, int target) {
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==target) return i;
+//         }
+
+//         return -1;
+//     }
+// };
+
+
+//the actual approach to be followed 
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==target) return i;
+        int st=0 , end=nums.size()-1;
+
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[st]<= nums[mid]){
+                if(nums[st]<=target && target<=nums[mid]){
+                    end= mid-1;
+
+                }else{
+                    st=mid+1;
+                } 
+
+            }
+            else{
+                if(nums[mid]<=target && target<= nums[end]){
+                    st=mid+1;
+                }
+                else{
+                    end=mid-1;
+                }
+            }
         }
 
         return -1;
