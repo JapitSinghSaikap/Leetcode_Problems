fd97f0ffad7dfb8ec7e97f95c79ed8bc37ebff8e
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-12 00:16:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -18,30 +18,3 @@ public:
         return maxx;
     }
 };
-
-
-// class Solution {
-// public:
-//     int maxArea(vector<int>& height) {
-//         int l = 0;
-//         int r = height.size() - 1;
-//         int area;
-//         int maxx = 0;
-        
-//         while (l <= r) {
-//             if (height[l] <= height[r]) {
-//                 l++;
-//                 area = height[l-1] * (r - l+1);
-//                 maxx = max(maxx, area);
-//             } else if (height[l] > height[r]) {
-//                 r--;
-//                 area = height[r] * (r - l);
-//                 maxx = max(maxx, area);
-//             }
-//         }
-//         l++;
-//         r--;
-        
-//         return maxx;
-//     }
-// };
