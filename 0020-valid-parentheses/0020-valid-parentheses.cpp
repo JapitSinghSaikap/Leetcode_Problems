4282d71edb7875b4a7d5c65e42cd3f13d57f5259
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-11 12:04:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,73 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-        for(int i=0; i<s.size(); i++){
-            if(s[i]=='(' || s[i]=='{' || s[i]=='['){//agr opening aayegi tbhi push krdo stack mein
+        int n=s.length();
+        stack<int> st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
                 st.push(s[i]);
-            }
-            else{
-                if(st.empty()) return false; // because uski opening bracket ho nhi skti as stk its empty
-                char ch = st.top(); // get the opening bracket to compare with the closing bracket
-                st.pop();//fir usko nikal bhi dena ke liye consider krdiya hai
-                if((s[i] == ')' and ch != '(') or  (s[i] == ']' and ch != '[') or (s[i] == '}' and ch != '{')) {
-                    return false;
-                }
+            }else{
+                if(st.empty()) return false;
+                char top=st.top();
+                st.pop();
+                if((s[i]==')' && top!='(') || (s[i]==']' && top!='[') || (s[i]=='}' && top!='{')) return false;
             }
         }
-        return st.empty(); 
+
+        return st.empty();
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         stack<char> st;
+//         for(int i=0; i<s.size(); i++){
+//             if(s[i]=='(' || s[i]=='{' || s[i]=='['){//agr opening aayegi tbhi push krdo stack mein
+//                 st.push(s[i]);
+//             }
+//             else{
+//                 if(st.empty()) return false; // because uski opening bracket ho nhi skti as stk its empty
+//                 char ch = st.top(); // get the opening bracket to compare with the closing bracket
+//                 st.pop();//fir usko nikal bhi dena ke liye consider krdiya hai
+//                 if((s[i] == ')' and ch != '(') or  (s[i] == ']' and ch != '[') or (s[i] == '}' and ch != '{')) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return st.empty(); 
+//     }
+// };
