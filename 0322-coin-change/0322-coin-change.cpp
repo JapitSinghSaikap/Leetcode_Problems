27f6410b2484da548ffb74c9842d9239cdf611cf
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-11 11:50:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int check(int n,vector<int>& arr,int target,vector<vector<int>>& dp){
-        if(n==0){
-            if(target%arr[n]==0) return target/arr[n];
-            else return 1e9;//itna bdha number do taki wo consider hi na ho
+    int check(int index,vector<int>& arr, int target,vector<vector<int>>& dp){
+        int n=arr.size();
+        if(index==n-1) {
+            if(target%arr[n-1]==0) return target/arr[n-1];
+            return 1e9;
         }
-        if (dp[n][target] != -1) return dp[n][target];
-        int nottake=check(n-1,arr,target,dp);
-        int take=INT_MAX;
-        //take only when target is greater than the current index value
-        if(target>=arr[n]){
-            take=1+check(n,arr,target-arr[n],dp);
+        if(dp[index][target]!=-1) return dp[index][target];
+        int notpick=check(index+1,arr,target,dp);
+        int pick=INT_MAX;
+        if(target>=arr[index]){
+            pick=1+check(index,arr,target-arr[index],dp);
         }
 
-        return dp[n][target] =  min(take,nottake);
+        return dp[index][target]=min(pick,notpick);
     }
-    int coinChange(vector<int>& arr, int target) {
+    int coinChange(vector<int>& arr, int target){
         int n=arr.size();
         vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        int ans=check(n-1,arr,target,dp);
+        int ans=check(0,arr,target,dp);
         if(ans>=1e9) return -1;
         return ans;
     }
@@ -26,21 +26,95 @@ public:
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // class Solution {
 // public:
-//     int coinChange(vector<int>& coins, int amount) {
-//         vector<int> minCoins(amount + 1, amount + 1);
-//         minCoins[0] = 0;
-
-//         for (int i = 1; i <= amount; i++) {
-//             for (int j = 0; j < coins.size(); j++) {
-//                 if (i - coins[j] >= 0) {
-//                     minCoins[i] = min(minCoins[i], 1 + minCoins[i - coins[j]]);
-//                 }
-//             }
+//     int check(int n,vector<int>& arr,int target,vector<vector<int>>& dp){
+//         if(n==0){
+//             if(target%arr[n]==0) return target/arr[n];
+//             else return 1e9;//itna bdha number do taki wo consider hi na ho
+//         }
+//         if (dp[n][target] != -1) return dp[n][target];
+//         int nottake=check(n-1,arr,target,dp);
+//         int take=INT_MAX;
+//         //take only when target is greater than the current index value
+//         if(target>=arr[n]){
+//             take=1+check(n,arr,target-arr[n],dp);
 //         }
 
-//         return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;        
+//         return dp[n][target] =  min(take,nottake);
+//     }
+//     int coinChange(vector<int>& arr, int target) {
+//         int n=arr.size();
+//         vector<vector<int>> dp(n,vector<int>(target+1,-1));
+//         int ans=check(n-1,arr,target,dp);
+//         if(ans>=1e9) return -1;
+//         return ans;
 //     }
 // };
+
+
+
+// // class Solution {
+// // public:
+// //     int coinChange(vector<int>& coins, int amount) {
+// //         vector<int> minCoins(amount + 1, amount + 1);
+// //         minCoins[0] = 0;
+
+// //         for (int i = 1; i <= amount; i++) {
+// //             for (int j = 0; j < coins.size(); j++) {
+// //                 if (i - coins[j] >= 0) {
+// //                     minCoins[i] = min(minCoins[i], 1 + minCoins[i - coins[j]]);
+// //                 }
+// //             }
+// //         }
+
+// //         return minCoins[amount] != amount + 1 ? minCoins[amount] : -1;        
+// //     }
+// // };
 
