36a7fd49e78fb53ad933baa5b9a34560e6520f5d
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-04-26 01:32:53 +0530

    Time: 25 ms (72.57%), Space: 16.3 MB (77.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,36 +1,81 @@
+// class Solution {
+// public:
+//     int r,c;
+//     bool valid(int i,int j){
+//         return i>=0 && i<r && j>=0 && j<c;
+//     }
+//     int numIslands(vector<vector<char>>& grid) {
+//         r = grid.size();
+//         c = grid[0].size();
+//         int row[4] = {1, -1, 0, 0};
+//         int col[4] = {0, 0, 1, -1};
+//         queue<pair<int,int>> q;
+//         int count = 0;
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 if(grid[i][j] == '1'){
+//                     count++;
+//                     q.push({i,j});
+//                     grid[i][j] = '0';//mark that spot as water taki count nhi kre wapis
+//                     while(!q.empty()){
+//                         int new_r = q.front().first;
+//                         int new_c = q.front().second;
+//                         q.pop();
+//                         for(int k=0;k<4;k++){
+//                             if(valid(new_r+row[k],new_c+col[k]) && grid[new_r+row[k]][new_c+col[k]] == '1'){
+//                                 grid[new_r+row[k]][new_c+col[k]] = '0';
+//                                 q.push({new_r+row[k],new_c+col[k]});
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
-    int r,c;
-    bool valid(int i,int j){
-        return i>=0 && i<r && j>=0 && j<c;
+    int r, c;
+
+    bool valid(int i, int j){
+        return i >= 0 && i < r && j >= 0 && j < c;
     }
+
+    void dfs(int i, int j, vector<vector<char>>& grid){
+        // mark visited
+        grid[i][j] = '0';
+
+        int row[4] = {1, -1, 0, 0};
+        int col[4] = {0, 0, 1, -1};
+
+        for(int k = 0; k < 4; k++){
+            int ni = i + row[k];
+            int nj = j + col[k];
+
+            if(valid(ni, nj) && grid[ni][nj] == '1'){
+                dfs(ni, nj, grid);
+            }
+        }
+    }
+
     int numIslands(vector<vector<char>>& grid) {
         r = grid.size();
         c = grid[0].size();
-        int row[4] = {1, -1, 0, 0};
-        int col[4] = {0, 0, 1, -1};
-        queue<pair<int,int>> q;
+
         int count = 0;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
+
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
                 if(grid[i][j] == '1'){
                     count++;
-                    q.push(make_pair(i,j));
-                    grid[i][j] = '0';
-                    while(!q.empty()){
-                        int new_r = q.front().first;
-                        int new_c = q.front().second;
-                        q.pop();
-                        for(int k=0;k<4;k++){
-                            if(valid(new_r+row[k],new_c+col[k]) && grid[new_r+row[k]][new_c+col[k]] == '1'){
-                                grid[new_r+row[k]][new_c+col[k]] = '0';
-                                q.push(make_pair(new_r+row[k],new_c+col[k]));
-                            }
-                        }
-                    }
+                    dfs(i, j, grid); 
                 }
             }
         }
+
         return count;
     }
 };
