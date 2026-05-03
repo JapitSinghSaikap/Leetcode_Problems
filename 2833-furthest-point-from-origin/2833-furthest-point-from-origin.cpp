dda93a0a4a50561319dd633fc954a3a9cac540de
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-05-03 14:37:34 +0530

    Time: 0 ms (100%), Space: 9.1 MB (26.92%) - LeetHub

--- 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
+++ 2833-furthest-point-from-origin/2833-furthest-point-from-origin.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int furthestDistanceFromOrigin(string s) {
-        int n=s.length();
-        int l=0;
-        int r=0;
-        int us=0;
-        for(int i=0;i<n;i++){
-            if(s[i]=='R') r++;
-            if(s[i]=='L') l++;
-            if(s[i]=='_') us++;
+        int n = s.length();
+        int l = 0;
+        int r = 0;
+        int us = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'R')
+                r++;
+            if (s[i] == 'L')
+                l++;
+            if (s[i] == '_')
+                us++;
         }
-        return abs(r-l)+us;
+        return abs(r - l) + us;
     }
 };
