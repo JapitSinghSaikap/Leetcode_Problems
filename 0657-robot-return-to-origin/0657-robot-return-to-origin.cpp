eddbad51876b0abf8a6857192de68f1dc58cd083
Japit302Saikap <142771494+JapitSinghSaikap@users.noreply.github.com>
2026-05-03 17:15:11 +0530

    Time: 4 ms (25.86%), Space: 10.2 MB (73.37%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -6,6 +6,7 @@ public:
             if(c=='R') axis.first++;
             else if(c=='L') axis.first--;
             else if(c=='U') axis.second++;
+            //down ke liye hai
             else axis.second--;
         }
         return axis.first==0 && axis.second ==0;
